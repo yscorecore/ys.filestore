fffc2c4960d9f876bacdbb85e1f2ed3024f7d4f0
pengbo.yang <obpt@sina.com>
2020-06-19 00:14:00 +0800

    add interface project

---
+++ src/YS.FileStore.Core/IFileStoreService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace YS.FileStore
+{
+    public interface IFileStoreService
+    {
+        Task<string> Put(Stream content, string fileKey, IDictionary<string, string> tags = default);
+        Task<string> GetUrl(string fileKey);
+        Task<bool> Update(string fileKey, IDictionary<string, string> tags);
+        Task<bool> Exists(string fileKey);
+        Task<bool> DeleteByKey(string fileKey);
+        Task<int> Count(IDictionary<string, string> tags);
+        Task<List<string>> ListKeys(Dictionary<string, string> tags, string startFileKey = default);
+        Task<IList<string>> DeleteBytags(Dictionary<string, string> tags, string startFileKey = default);
+    }
+
+}
